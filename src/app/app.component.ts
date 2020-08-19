e38fa85853dae3476c9c29e7b99c12e450f1e231
Suguru Inatomi <suguru.inatomi@gmail.com>
2020-08-19 10:47:22 +0900

    feat(app): show app version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faEraser, faPen, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { VERSION } from './version';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,8 @@ import { faEraser, faPen, faTimes } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  readonly version = VERSION;
+
   ngOnInit(): void {
     library.add(faEraser, faPen, faTimes);
     dom.watch();
